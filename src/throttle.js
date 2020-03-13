2e65bfdbfeadbc222f50cbbe5fed77ac1737c812
TimRChen <timrchen@foxmail.com>
2020-03-13 18:25:30 +0800

    节流函数的三种实现方式

--- src/throttle.js
+++ src/throttle.js
@@ -1,7 +1,5 @@
 const $box = document.querySelector(".mouse-move-box");
 
-let timer = null;
-
 const options = {
   capture: false
 };
@@ -12,11 +10,35 @@ const listener = e => {
   console.log(str);
 };
 
-// throttle 要点：
-//  1. 时效性，一段时间内重复执行是无效的。
-//  2. 连续性，连续执行的事件适合节流场景。
-//  3. 函数实现上采用科里化思想
+// 别人的面试遇到的问题：能实现一个节流函数吗？
+// 别人的回答：(很快写完了 定时器版) 面试官改进一下要求滚动第一次就触发，于是改成时间戳版。
+// 再改进一下，最后一次的延迟也要触发，于是改成了时间戳+定时器版，完美解决
+// 特此，我对节流进行了一次分析，对上述三种情况下的节流进行了不同的实现：
+
+// > 定时器版:（特点：可以触发最后一次事件的延迟，缺点：无法对事件第一次响应进行触发）
 function throttle(fn, wait) {
+  let timer = null;
+  return e => {
+    if (!timer) {
+      timer = setTimeout(() => (fn.call(this, e), (timer = null)), wait);
+    }
+  };
+}
+
+// > 时间戳版:（特点：事件第一次响应触发，缺点：最后一次事件延迟无法触发）
+function  throttle(fn, wait) {
+  let start = Date.now();
+  return e => {
+    if (Date.now() - start >= wait) {
+      fn.call(this, e);
+      start = Date.now()
+    }
+  }
+}
+
+// > 时间戳+定时器版
+function throttle(fn, wait) {
+  let timer = null;
   let start = Date.now();
   return e => {
     if (Date.now() - start >= wait) {
@@ -27,6 +49,11 @@ function throttle(fn, wait) {
   };
 }
 
-const wait = 100;
+// throttle 要点：
+//  1. 时效性，一段时间内重复执行是无效的。
+//  2. 连续性，连续执行的事件适合节流场景。
+//  3. 函数实现上采用科里化思想
+
+const wait = 100 * 10; // 1s 时间长比较容易看出这三种节流实现的区别
 
 $box.addEventListener("mousemove", throttle(listener, wait), options);
